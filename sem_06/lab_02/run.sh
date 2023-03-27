f0b2d4069efeeed97fa64398756180d2bce4aea7
Natasha Gurova <78147880+Tulenenok@users.noreply.github.com>
2023-03-27 16:53:59 +0300

    ---

---
+++ sem_06/lab_02/run.sh
@@ -0,0 +1,14 @@
+# arg=$1
+# if [ "$1" == "1" ]; then
+#     cd task_01
+#     bash run.sh
+# elif [ "$1" == "2" ]; then
+#     cd task_02
+#     bash run.sh
+# fi
+gcc client.c -o client
+gcc server.c -o server
+# open server
+# open client
+# open client
+# open client
